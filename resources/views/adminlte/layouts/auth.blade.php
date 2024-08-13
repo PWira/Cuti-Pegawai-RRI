fa26649351f56e4db28e2a49320f90f283894a4e
pwira1 <personalputra06@gmail.com>
2024-08-13 07:54:19 +0700

    new changes

--- resources/views/adminlte/layouts/auth.blade.php
+++ resources/views/adminlte/layouts/auth.blade.php
@@ -12,7 +12,7 @@
   <!-- icheck bootstrap -->
   <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/adminlte.min.css') }}">
 </head>
 @yield('content')
 
@@ -21,6 +21,6 @@
 <!-- Bootstrap 4 -->
 <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('assets/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('assets/js/adminlte.min.js') }}"></script>
 </body>
 </html>
